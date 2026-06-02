55c5946c688a6ee693ff77ed3f01350e178415c4
surendravattiprolu <balasurendravattiprolu@gmail.com>
2026-06-02 15:04:28 +0530

    Update webasg.tf

--- webasg.tf
+++ webasg.tf
@@ -14,7 +14,7 @@ resource "aws_autoscaling_group" "swiggy-web-asg" {
 ###### Create a Launch Template for the EC2 instances ######
 resource "aws_launch_template" "swiggy-web-template" {
   name_prefix   = "swiggy-web-template"
-  image_id      = "ami-0f559c3642608c138"
+  image_id      = "ami-00e801948462f718a"
   instance_type = "t3.small"
 
   network_interfaces {
