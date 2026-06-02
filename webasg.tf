c723165c16850d5b1bfc5d958aa409661faffdb7
surendravattiprolu <balasurendravattiprolu@gmail.com>
2026-06-02 19:00:23 +0530

    Update AMI ID in launch template configuration

--- webasg.tf
+++ webasg.tf
@@ -14,7 +14,7 @@ resource "aws_autoscaling_group" "swiggy-web-asg" {
 ###### Create a Launch Template for the EC2 instances ######
 resource "aws_launch_template" "swiggy-web-template" {
   name_prefix   = "swiggy-web-template"
-  image_id      = "ami-091138d0f0d41ff90"
+  image_id      = "ami-0685bcc683dadb6b9"
   instance_type = "t3.small"
 
   network_interfaces {
